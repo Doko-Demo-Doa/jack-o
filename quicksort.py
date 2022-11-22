adf937482ebede8825e1a7c2f5eb902b9f1b3f22
Quan Pham <doraemonfanclub@gmail.com>
2022-11-22 09:55:49 +0700

    Complete script

--- quicksort.py
+++ quicksort.py
@@ -11,4 +11,39 @@ def partition(array: list[int], low: int, high: int):
             # If element smaller than pivot is found
             # then we swap it with the greater element pointed by i
             i = i + 1
-    pass
+
+            # swapping element at j with element at j
+            (array[i], array[j]) = (array[j], array[i])
+
+    # swap the pivot element with the greater element specified by i
+    (array[i+1], array[high]) = (array[high], array[i+1])
+
+    return i + 1
+
+# function to perform sorting
+
+
+def quick_sort(array: list[int], low: int, high: int):
+    if low < high:
+        # find pivot element so that:
+        # element smaller than pivot are on the left
+        # element greater than pivot are on the right
+        pi = partition(array, low, high)
+
+        # recursive call on the left
+        quick_sort(array, low, pi - 1)
+
+        # recursive call on the right
+        quick_sort(array, pi + 1, high)
+
+
+data = [8, 7, 2, 1, 0, 9, 6]
+print("Unsorted Array")
+print(data)
+
+size = len(data)
+
+quick_sort(data, 0, size - 1)
+
+print("sorted data in ascending order")
+print(data)
